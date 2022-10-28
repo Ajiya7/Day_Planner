cc49e7dadb148371817ab1d6aad58876896938a4
Faruk Ajiya <Ajiyafaruk@gmail.com>
2022-10-27 22:03:09 -0700

    color cahnge of the textarea according to the time of day is completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,22 @@
+var textareacolor = document.querySelectorAll("textarea");
+
+// TO DISPLAY THE CURRENT DATE
 var day = moment().format("dddd, Do MMMM YYYY");
 $("#currentDay").text(day); 
+
+// TO STORE THE CURRENT HOUR OF THE DAY IN THE HOUR VARAIBLE
+var hour = moment().hour();
+
+// TO CHANGE THE COLOR OFTHE TECTAREA SEPENDIDNG ON THE TIME  OF THE DAY
+console.log(textareacolor)
+for (let i = 0; i < textareacolor.length; i++) {
+    var time = textareacolor[i].dataset.time 
+    var htime = parseInt(time)
+    if (hour > htime) {
+        textareacolor[i].classList.add('past')
+    } else if (hour == htime) {
+        textareacolor[i].classList.add('present')
+    } else {
+        textareacolor[i].classList.add('future');
+    }
+}
